33f9859c6629d5b19f9a7762355819847e33d311
tamerahmed166 <tamer.ahmed166@gmail.com>
2025-09-26 22:32:31 +0300

    Add files via upload

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,14 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'error'
   },
 }
